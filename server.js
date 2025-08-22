d908ff7be50b62e65c5f0816ba758f4f9ab41d42
Dreamer609 <asifbanglades@gmail.com>
2025-08-23 00:01:47 +0600

    Added base url

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 3000;
 const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 
 const server = http.createServer((req, res) => {
+
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-type": "text/plain" });
+    res.end("Welcome!");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+
   if (req.method === "GET" && req.url === "/resource/putSVG") {
     aquireSVG(req, res);
   } else {
